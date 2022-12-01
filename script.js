47e9129997397066a7cdd06d6a9e00c265335a9f
learningwub <learningwebdev.d1q7e@simplelogin.com>
2022-12-01 22:36:30 +0530

    Minor changes to the landing page.

---
+++ script.js
@@ -0,0 +1,10 @@
+// const scroll = document.querySelector('#scroll');
+// console.log(scroll);
+
+// function hide() {
+//   scroll.style.display = 'none';
+//   console.log("it's listening");
+// };
+
+// window.addEventListener('scroll', hide);
+// console.log('hello');
