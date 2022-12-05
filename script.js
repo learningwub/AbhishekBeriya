48d24ec5c6fe9e02c8f27f75a4398d62d88383b7
learningwub <learningwebdev.d1q7e@simplelogin.com>
2022-12-05 21:16:18 +0530

    openMenu button working.

--- script.js
+++ script.js
@@ -8,3 +8,23 @@
 
 // window.addEventListener('scroll', hide);
 // console.log('hello');
+console.log('its linked');
+const openMenu = document.getElementById("openMenu");
+console.log(openMenu);
+function Menu() {
+  document.getElementById('homePage').style.overflow = "hidden";
+  document.getElementById('homePage').style.backgroundImage = "none";
+  document.getElementById('intro').style.display = "none";
+  document.getElementById('scroll').style.display = "none"; 
+  document.getElementById('openMenu').style.display = "none"; 
+
+  document.getElementById('menu').style.display = 'flex';
+  document.getElementById('closeMenu').style.display = 'flex';
+  // document.getElementById('nav').style.scale = '0.85';
+  document.getElementById('nav').style.transform = 'scale(0.85)';
+  console.log('Menu')
+}
+document.getElementById("openMenu").addEventListener('click', Menu);
+document.getElementById("openMenu").addEventListener('mouseover', Menu);
+// document.getElementById("closeMenu").addEventListener('click', Menu);
+// document.getElementById("closeMenu").addEventListener('mouseover', Menu);
